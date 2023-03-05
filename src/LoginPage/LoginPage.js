72f5a7ec1c89c9c6dbff5949e00569a81f83e204
yucpu <985968134@qq.com>
2023-03-06 02:26:49 +0800

    adding Network graph

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {ForceGraph3D} from 'react-force-graph';
+import SpriteText from 'three-spritetext';
 
 function LoginPage() {
+
+  let links = [{source:"1", target:"2"},{source:"2", target:"3"},{source:"4", target:"1"},{source:"4", target:"2"}]
+  let data = { nodes: [{id:"1"},{id:"2"},{id:"3"},{id:"4"}], links: links }
   return (
-    <div>LoginPage</div>
+    <ForceGraph3D graphData={data} 
+    nodeThreeObject={node=> {const sprite = new SpriteText(node.id); sprite.color = "red"; return sprite}} 
+    nodeThreeObjectExtend={false}
+    />
   )
 }
 
