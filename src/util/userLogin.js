6563160ca910a340e7a621ae2cf70f3fae4357db
yucpu <985968134@qq.com>
2023-03-11 09:03:13 +0800

    back to prev version

--- src/util/userLogin.js
+++ src/util/userLogin.js
@@ -1,27 +1,30 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useData } from '../data'
-import { Button, Input } from 'antd'
+import { Button } from 'antd'
 
 function UserLogin() {
-    const context = useData();
-    const [isHover, setHover] = useState(false);
-    const handleAuth =()=>{
-        if(context.user){
-          context.setUser(null)
-        }else{
-          context.setUser("12dc1a")
-        }
-      }
-    return(
-        <Button className="app_user" shape='round' size='large' 
-        onMouseLeave={()=>setHover(false)} 
-        onMouseEnter={()=>setHover(true)}
+  const context = useData();
+  const [isHover, setHover] = useState(false);
+  const handleAuth = () => {
+    if (context.user) {
+      context.setUser(null)
+    } else {
+      context.setUser("12dc1a")
+    }
+  }
+  return useMemo(() => {
+    return (
+      <Button className="app_user" shape='round' size='large'
+        onMouseLeave={() => setHover(false)}
+        onMouseEnter={() => setHover(true)}
         onClick={handleAuth}
-        icon={isHover ? "":context.user ? "Q":"W"}
-        >
-          {isHover ? context.user ? "Log out":"Log in":""}
-        </Button>
-    )
-}
+        icon={isHover? "":context.user ? "Q":"W"}
+      >
+        {isHover ? context.user ? "Log out": "Log in":""}
 
+      </Button>
+    )
+  }, [context.user, isHover])
+
+}
 export default UserLogin
