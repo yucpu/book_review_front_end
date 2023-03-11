09a70ebe8542519b2f6e47400e7cbad2f36ca5dd
yucpu <985968134@qq.com>
2023-03-11 09:01:23 +0800

    add pagination to comments page

--- src/BookDetail/Comments.js
+++ src/BookDetail/Comments.js
@@ -7,28 +7,42 @@ import { useData } from '../data';
 function Comments() {
     const context = useData();
 
+    const pagination = {
+        position: 'bottom',
+        align: 'center',
+        pageSize: 10,
+        size: 'default',
+        total: context.comments.length,
+        showSizeChanger: false
+      }
 
     return useMemo(() => {
         return (
             <Modal id='BookDetail' open={context.commentShow} 
             onCancel={() => context.setCommentShow(false)}
             footer={null}
+            width={"50%"}
+            bodyStyle={{height:"500px"}}
+            title="Reader Comments"
             >
                 <List
                     itemLayout="horizontal"
+                    pagination={pagination}
+                    split={true}
                     dataSource={context.comments}
                     renderItem={(item, index) => (
                         <List.Item>
                             <List.Item.Meta
                                 avatar={<Avatar src={require("../resources/gamer.png")} />}
-                                title={<a href="https://ant.design">{item.user}</a>}
-                                description={item.comments}
+                                title={item.user}
+                                description={item.comment}
                             />
+                            {item.comment}
                         </List.Item>
                     )}
                 />
             </Modal>
         )
-    }, [context.book, context.commentShow])
+    }, [context.book, context.commentShow, context.comments])
 }
 export default Comments;
