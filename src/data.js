4d40809fc9c7a0fc71ce08b42767a316225a5d51
yucpu <985968134@qq.com>
2023-03-11 09:00:55 +0800

    updates Comments page UI

--- src/data.js
+++ src/data.js
@@ -4,6 +4,14 @@ export const DataContext = createContext(null);
 const {Provider} = DataContext;
 export const serverHost = 'https://769c8b6b-fb9c-4090-91ce-c4b37a9545be.mock.pstmn.io/'
 
+const autoSuggestionCig = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'd7ab2fd67bmsh64a9a4f50904490p121abdjsn8e3d8a805b0b',
+      'X-RapidAPI-Host': 'auto-suggest-queries.p.rapidapi.com'
+    }
+  };
+
 export const DataProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [method,setMethod] = useState("phrase");
@@ -14,7 +22,7 @@ export const DataProvider = ({children}) => {
     const [loading, setLoading] = useState(false);
     const [graph,setGraph] = useState({});
     const [commentShow, setCommentShow] = useState(false);
-    const [comments, setComments] = useState([]);
+    const [comments, setComments] = useState([{user:"TTDS", comment:"Hard to read"}]);
     const login = (user) => {
         setUser(user)
     }
@@ -33,7 +41,7 @@ export const DataProvider = ({children}) => {
         selectMethod, options, setOptions, query, 
         setQuery, result,setResult,
         num_res, setNum_res, loading,setLoading,
-        commentShow, setCommentShow,
+        commentShow, setCommentShow, comments, setComments,
         graph,setGraph}}>
             {children}
         </Provider>
@@ -73,3 +81,9 @@ export async function postData(url = '', data={}, load){
     return response;
 }
 
+export async function getSuggestion(query){
+    let url = 'https://auto-suggest-queries.p.rapidapi.com/suggestqueries?query='+query;
+
+    const response = await fetch(url, autoSuggestionCig);
+    return response.json();
+}
